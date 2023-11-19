1fc6aef2973d04e966ab6ab1d80140edc4bdac92
gradientick <camus.luis95@gmail.com>
2023-11-19 22:02:04 +0800

    added some changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/Gradientick/Digital-Museum",
+  base: "https://gradientick.github.io/Digital-Museum",
   plugins: [react()],
 });
